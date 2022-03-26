35887c388d7faceed52e1d5c3e445d0e7b107334
Jack Homan <homan.jack@gmail.com>
2022-03-26 19:37:45 -0400

    strings

---
+++ literal.go
@@ -0,0 +1,45 @@
+package parser
+
+type LiteralType string
+const (
+    StringLiteralType LiteralType = "STRING_LITERAL"
+    IntLiteralType    LiteralType = "INT_LITERAL"
+)
+
+
+type Literal interface {
+    Type() LiteralType
+    Value() interface{}
+}
+
+type IntLiteral struct {
+    value int
+}
+
+func (lit *IntLiteral) Value() interface{} {
+    return lit.value
+}
+
+func (lit *IntLiteral) Type() LiteralType {
+    return IntLiteralType
+}
+
+type StringLiteral struct {
+    value string
+}
+
+func (lit *StringLiteral) Value() interface{} {
+    return lit.value
+}
+
+func (lit *StringLiteral) Type() LiteralType {
+    return StringLiteralType
+}
+
+func repr(literal Literal) map[string]interface{} {
+    return map[string]interface{}{
+        "Type": literal.Type(),
+        "Value": literal.Value(),
+    }
+
+}
