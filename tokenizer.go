6b4eddc8ba8db377dc58c193c3f73749e091e6ef
Jack Homan <homan.jack@gmail.com>
2022-03-27 18:04:23 -0400

    refactor tests

--- tokenizer.go
+++ tokenizer.go
@@ -1,258 +1,341 @@
 package parser
 
 import (
-    "fmt"
-    "regexp"
-    "strconv"
+	"fmt"
+	"regexp"
+	"strconv"
 )
 
-var specs = map[string]LiteralType{
-    `(^\d+)`: IntLiteralType,
-    `^"([^"]*)"`: StringLiteralType,
-    `^\s+`: WhitespaceLiteral,
-    `^//.*`: CommentLiteral,
-    `^/\*[\s\S]*?\*/`: CommentLiteral,
-    `^;`: ExpressionTerm,
+type TokenType int
+
+const (
+	IntegerLiteralRegex      = `(^\d+)`
+	StringLiteralRegex       = `^"([^"]*)"`
+	WhitespaceRegex          = `^\s+`
+	CommentRegex             = `^//.*`
+	BlockCommentRegex        = `^/\*[\s\S]*?\*/`
+	TerminateExpressionRegex = `^;`
+	BlockStatementRegex      = `^{`
+	BlockStatementEndRegex   = `^}`
+
+	IntegerLiteralToken TokenType = iota + 1
+	StringLiteralToken
+	NullToken
+	TerminateLiteralToken
+	BlockStatementToken
+	BlockStatementEndToken
+)
+
+func (tt TokenType) String() string {
+    switch tt {
+    case IntegerLiteralToken:
+        return "IntegerLiteral"
+    case StringLiteralToken:
+        return "StringLiteral"
+    case NullToken:
+        return "Null"
+    case TerminateLiteralToken:
+        return "TerminateExpression"
+    case BlockStatementToken:
+        return "BlockStatementStart"
+    case BlockStatementEndToken:
+        return "BlockStatementEnd"
+    default:
+        return fmt.Sprintf("%d", int(tt))
+    }
+}
+
+var specs = map[string]TokenType{
+	IntegerLiteralRegex:      IntegerLiteralToken,
+	StringLiteralRegex:       StringLiteralToken,
+	WhitespaceRegex:          NullToken,
+	CommentRegex:             NullToken,
+	BlockCommentRegex:        NullToken,
+	TerminateExpressionRegex: TerminateLiteralToken,
+	BlockStatementRegex:      BlockStatementToken,
+	BlockStatementEndRegex:   BlockStatementEndToken,
 }
 
 // Lexical analysis
 
 type SyntaxError struct {
-    message string
+	message string
 }
 
 func (err *SyntaxError) Error() string {
-    return err.message
+	return err.message
 }
 
 func NewSyntaxError(message string) error {
-    return &SyntaxError{message}
+	return &SyntaxError{message}
 }
 
 type Expression struct {
-    Literal Literal
+	Literal Literal
 }
 
 type ExpressionStatement struct {
-    Expression Expression
+	Expression Expression
+}
+
+type BlockStatement struct {
+	StatementList StatementList
 }
 
 type Statement struct {
-    ExpressionStatement ExpressionStatement
+	ExpressionStatement *ExpressionStatement
+	BlockStatement      *BlockStatement
 }
 
 type StatementList struct {
-    Statements []Statement
-    StatementList *StatementList
+	Statements    []Statement
+	StatementList *StatementList
 }
 
 type Program struct {
-    Type string
-    Body StatementList
+	Type string
+	Body StatementList
 }
 
 type Parser interface {
-    // Parse parses a string into an abstract syntax tree (AST)
-    Parse(s string) (Program, error)
+	// Parse parses a string into an abstract syntax tree (AST)
+	Parse(s string) (Program, error)
 
-    // Program
-    //   ; StatementList
-    //   ;
-    Program() (Program, error)
+	// Program
+	//   ; StatementList
+	//   ;
+	Program() (Program, error)
 
-    // StatementList
-    //   : Statement
-    //   | StatementList Statement
-    //   ;
-    StatementList() (StatementList, error)
+	// StatementList
+	//   : Statement
+	//   | StatementList Statement
+	//   ;
+	StatementList(TokenType) (StatementList, error)
 
-    // Statement
-    //   : ExpressionStatement
-    //   ;
-    Statement() (Statement, error)
+	// Statement
+	//   : ExpressionStatement
+	//   | BlockStatement
+	//   ;
+	Statement() (Statement, error)
 
-    // ExpressionStatement
-    //   : Expression ;
-    //   ;
-    ExpressionStatement() (ExpressionStatement, error)
+	// ExpressionStatement
+	//   : Expression ;
+	//   ;
+	ExpressionStatement() (Statement, error)
 
-    // Expression
-    //   : Literal
-    //   ;
-    Expression() (Expression, error)
+	// BlockStatement
+	//   : '{' StatementList '}'
+	//   ;
+	BlockStatement() (Statement, error)
 
-    // Literal
-    //   : StringLiteral
-    //   | NumericLiteral
-    //   ;
-    Literal() (Literal, error)
+	// Expression
+	//   : Literal
+	//   ;
+	Expression() (Expression, error)
 
-    // StringLiteral
-    //   ; string
-    StringLiteral() (*StringLiteral, error)
-
-    // IntLiteral
-    //   ; int
-    //   ;
-    IntLiteral() (*IntLiteral, error)
+	// Literal
+	//   : StringLiteral
+	//   | NumericLiteral
+	//   ;
+	Literal() (Literal, error)
 }
 
 type parser struct {
-    tokenizer *tokenizer
-    lookAhead Token
+	tokenizer *tokenizer
+	lookAhead Token
 }
 
 // Parse the string s into an abstract syntax tree
 func (p *parser) Parse(s string) (Program, error) {
-    p.tokenizer = NewTokenizer(s)
-    p.lookAhead, _ = p.tokenizer.NextToken()
-    return p.Program()
+	p.tokenizer = NewTokenizer(s)
+	p.lookAhead, _ = p.tokenizer.NextToken()
+	return p.Program()
 }
 
 func (p *parser) Expression() (Expression, error) {
-    lit, err := p.Literal()
-    if err != nil {
-        return Expression{}, err
-    }
-    _, err = p.eat(ExpressionTerm)
-    if err != nil {
-        return Expression{}, err
-    }
-    return Expression{Literal: lit}, nil
+	lit, err := p.Literal()
+	if err != nil {
+		return Expression{}, err
+	}
+	_, err = p.eat(TerminateLiteralToken)
+	if err != nil {
+		return Expression{}, err
+	}
+	return Expression{Literal: lit}, nil
 }
 
-func (p *parser) ExpressionStatement() (ExpressionStatement, error) {
-    expression, err := p.Expression()
-    if err != nil {
-        return ExpressionStatement{}, err
-    }
-    return ExpressionStatement{Expression: expression}, nil
+func (p *parser) ExpressionStatement() (Statement, error) {
+	expression, err := p.Expression()
+	if err != nil {
+		return Statement{}, err
+	}
+	return Statement{
+		ExpressionStatement: &ExpressionStatement{Expression: expression},
+	}, nil
 }
 
-func (p *parser) Statement() (Statement, error) {
-    expressionStatement, err := p.ExpressionStatement()
+func (p *parser) BlockStatement() (Statement, error) {
+    _, err := p.eat(BlockStatementToken)
     if err != nil {
         return Statement{}, err
     }
-    return Statement{ExpressionStatement: expressionStatement}, nil
+	block := &BlockStatement{}
+	if p.lookAhead.Type != BlockStatementEndToken {
+		var err error
+		block.StatementList, err = p.StatementList(BlockStatementEndToken)
+		if err != nil {
+			return Statement{}, err
+		}
+	} else {
+		block.StatementList = StatementList{}
+	}
+	_, err = p.eat(BlockStatementEndToken)
+	if err != nil {
+		return Statement{}, err
+	}
+	return Statement{BlockStatement: block}, nil
 }
 
-func (p *parser) StatementList() (StatementList, error) {
-    statements := StatementList{}
+func (p *parser) Statement() (Statement, error) {
+	switch p.lookAhead.Type {
+	case BlockStatementToken:
+		return p.BlockStatement()
+	default:
+		return p.ExpressionStatement()
+	}
+}
 
-    for !p.lookAhead.IsEmpty() {
-        statement, err := p.Statement()
-        if err != nil {
-            return StatementList{}, err
-        }
-        statements.Statements = append(statements.Statements, statement)
-    }
-    return statements, nil
+func (p *parser) StatementList(stop TokenType) (StatementList, error) {
+	statements := StatementList{}
+
+	for !p.lookAhead.IsEmpty() && p.lookAhead.Type != stop {
+		statement, err := p.Statement()
+		if err != nil {
+			return StatementList{}, err
+		}
+		statements.Statements = append(statements.Statements, statement)
+	}
+	return statements, nil
 }
 
 func (p *parser) Program() (Program, error) {
-    statements, err := p.StatementList()
-    if err != nil {
-        return Program{}, err
-    }
-    return Program{Type: "Program", Body: statements}, nil
+	statements, err := p.StatementList(TokenType(0))
+	if err != nil {
+		return Program{}, err
+	}
+	return Program{Type: "Program", Body: statements}, nil
 }
 
-func (p *parser) eat(tokenType LiteralType) (Token, error) {
-    token := p.lookAhead
-    if token.IsEmpty() {
-        return token, NewSyntaxError("EOF")
+func (p *parser) eat(tokenType TokenType) (Token, error) {
+	token := p.lookAhead
+	if token.IsEmpty() {
+		return token, NewSyntaxError("EOF")
+	}
+	if token.Type != tokenType {
+		return token, NewSyntaxError(fmt.Sprintf("unexpected token: '%s'", token.Value))
+	}
+    var err error
+	p.lookAhead, err = p.tokenizer.NextToken()
+    if err != nil {
+        return Token{}, err
     }
-    if token.Type != string(tokenType) {
-        return token, NewSyntaxError(fmt.Sprintf("unexpected token: '%s'", token.Value))
-    }
-    p.lookAhead, _ = p.tokenizer.NextToken()
-    return token, nil
+	return token, nil
 }
 
-func (p *parser) IntLiteral() (*IntLiteral, error) {
-    token, err := p.eat(IntLiteralType)
-    if err != nil {
-        return nil, err
-    }
-    i, _ := strconv.Atoi(token.Value)
-    return &IntLiteral{i}, nil
+func (p *parser) IntLiteral() (Literal, error) {
+	token, err := p.eat(IntegerLiteralToken)
+	if err != nil {
+		return Literal{}, err
+	}
+	i, _ := strconv.Atoi(token.Value)
+	return Literal{Value: i}, nil
 }
 
-func (p *parser) StringLiteral() (*StringLiteral, error) {
-    token, err := p.eat(StringLiteralType)
-    if err != nil {
-        return nil, err
-    }
-    return &StringLiteral{token.Value}, nil
+func (p *parser) StringLiteral() (Literal, error) {
+	token, err := p.eat(StringLiteralToken)
+	if err != nil {
+		return Literal{}, err
+	}
+	return Literal{Value: token.Value}, nil
 }
 
 func (p *parser) Literal() (Literal, error) {
-    switch LiteralType(p.lookAhead.Type) {
-    case StringLiteralType: { return p.StringLiteral() }
-    case IntLiteralType: { return p.IntLiteral() }
-    default: { return nil, NewSyntaxError(fmt.Sprintf("Invalid literal type %s", p.lookAhead.Type)) }
-    }
+	switch p.lookAhead.Type {
+	case StringLiteralToken:
+		{
+			return p.StringLiteral()
+		}
+	case IntegerLiteralToken:
+		{
+			return p.IntLiteral()
+		}
+	default:
+		{
+			return Literal{}, NewSyntaxError(fmt.Sprintf("Invalid literal type %s", p.lookAhead.Type))
+		}
+	}
 }
 
 var _ Parser = &parser{}
 
 func New() *parser {
-    return &parser{}
+	return &parser{}
 }
 
 type Token struct {
-    Type string
-    Value string
+	Type  TokenType
+	Value string
 }
 
 func (tok *Token) IsEmpty() bool {
-    return tok.Type == ""
+	return tok.Type == 0
 }
 
 type String string
 
 func (s String) Len() int {
-    return len(s)
+	return len(s)
 }
 
 func (s String) Slice(start int) String {
-    return s[start:]
+	return s[start:]
 }
 
 type tokenizer struct {
-    String
-    cursor int
+	String
+	cursor int
 }
 
 func (tok *tokenizer) hasMoreTokens() bool { return tok.cursor < tok.String.Len() }
 
 func (tok *tokenizer) NextToken() (Token, error) {
-    if !tok.hasMoreTokens() {
-        return Token{}, nil
+    skip := map[TokenType]bool {
+        TerminateLiteralToken: false,
+        BlockStatementToken: false,
+        BlockStatementEndToken: false,
     }
-    for pattern, literalType := range specs {
-        str := string(tok.String.Slice(tok.cursor))
+	if !tok.hasMoreTokens() {
+		return Token{}, nil
+	}
+	for pattern, literalType := range specs {
+		str := string(tok.String.Slice(tok.cursor))
 
-        re := regexp.MustCompile(pattern)
-        if re.MatchString(str) {
-            match := re.FindStringSubmatch(str)
-            tok.cursor += len(match[0])
-            if literalType == WhitespaceLiteral {
-                return tok.NextToken()
+		re := regexp.MustCompile(pattern)
+		if re.MatchString(str) {
+			match := re.FindStringSubmatch(str)
+			tok.cursor += len(match[0])
+			if literalType == NullToken {
+				return tok.NextToken()
+			}
+            if _, ok := skip[literalType]; ok {
+                return Token{Type: literalType}, nil
             }
-            if literalType == CommentLiteral {
-                return tok.NextToken()
-            }
-            if literalType == ExpressionTerm {
-                return Token{Type: string(literalType)}, nil
-            }
-            return Token{Type: string(literalType), Value: match[1]}, nil
-        }
-    }
-    return Token{}, NewSyntaxError(fmt.Sprintf(`Unexpected token: %c`, tok.String[0]))
+			return Token{Type: literalType, Value: match[1]}, nil
+		}
+	}
+	return Token{}, NewSyntaxError(fmt.Sprintf(`Unexpected token: %c`, tok.String[0]))
 }
 
 func NewTokenizer(s string) *tokenizer {
-    return &tokenizer{String(s), 0}
+	return &tokenizer{String(s), 0}
 }
--- tokenizer_test.go
+++ tokenizer_test.go
@@ -1,63 +1,160 @@
 package parser_test
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 
-    "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"
 
-    "parser"
+	"parser"
 )
 
 func TestParser_IntLiteral(t *testing.T) {
-    expected := 42
-    ast, err := parser.New().Parse(fmt.Sprintf(`%d;`, expected))
-    require.Nil(t, err)
-    lit := ast.Body.Statements[0].ExpressionStatement.Expression.Literal
-    require.Equal(t, lit.Type(), parser.IntLiteralType)
-    require.Equal(t, lit.Value(), expected)
+	expected := 42
+	ast, err := parser.New().Parse(fmt.Sprintf(`%d;`, expected))
+	require.Nil(t, err)
+    expectedAst := parser.StatementList{
+        Statements: []parser.Statement{{
+            ExpressionStatement: &parser.ExpressionStatement{
+                Expression: parser.Expression{
+                    Literal: parser.Literal{Value: 42},
+                },
+            }},
+        },
+    }
+	require.Equal(t, ast.Body, expectedAst)
 }
 
 func TestParser_IntMultiple(t *testing.T) {
-    expected1 := 42
-    expected2 := 43
-    ast, err := parser.New().Parse(fmt.Sprintf("%d;\n%d;", expected1, expected2))
+	expected1 := 42
+	expected2 := 43
+	ast, err := parser.New().Parse(fmt.Sprintf("%d;\n%d;", expected1, expected2))
     require.Nil(t, err)
-    for k, exp := range []int{expected1, expected2} {
-        lit := ast.Body.Statements[k].ExpressionStatement.Expression.Literal
-        require.Equal(t, lit.Type(), parser.IntLiteralType)
-        require.Equal(t, lit.Value(), exp)
+    expectedAst := parser.StatementList{
+        Statements: []parser.Statement{
+            {
+                ExpressionStatement: &parser.ExpressionStatement{
+                    Expression: parser.Expression{
+                        Literal: parser.Literal{Value: expected1},
+                    },
+                },
+            },
+            {
+                ExpressionStatement: &parser.ExpressionStatement{
+                    Expression: parser.Expression{
+                        Literal: parser.Literal{Value: expected2},
+                    },
+                },
+            },
+        },
     }
+    require.Equal(t, ast.Body, expectedAst)
 }
 
 func TestParser_StringLiteral(t *testing.T) {
-    expected := "string literal"
-    ast, err := parser.New().Parse(fmt.Sprintf(`"%s";`, expected))
-    require.Nil(t, err)
-    lit := ast.Body.Statements[0].ExpressionStatement.Expression.Literal
-    require.Equal(t, lit.Type(), parser.StringLiteralType)
-    require.Equal(t, lit.Value(), expected)
+	expected := "string literal"
+	ast, err := parser.New().Parse(fmt.Sprintf(`"%s";`, expected))
+	require.Nil(t, err)
+    expectedAst := parser.StatementList{
+        Statements: []parser.Statement{{
+            ExpressionStatement: &parser.ExpressionStatement{
+                Expression: parser.Expression{
+                    Literal: parser.Literal{Value: "string literal"},
+                },
+            }},
+        },
+    }
+    require.Equal(t, ast.Body, expectedAst)
 }
 
 func TestParser_Whitespace(t *testing.T) {
-    expected := 42
-    ast, err := parser.New().Parse(fmt.Sprintf(`     %d;`, expected))
-    require.Nil(t, err)
-    lit := ast.Body.Statements[0].ExpressionStatement.Expression.Literal
-    require.Equal(t, lit.Type(), parser.IntLiteralType)
-    require.Equal(t, lit.Value(), expected)
+	expected := 42
+	ast, err := parser.New().Parse(fmt.Sprintf(`     %d;`, expected))
+	require.Nil(t, err)
+    expectedAst := parser.StatementList{
+        Statements: []parser.Statement{{
+            ExpressionStatement: &parser.ExpressionStatement{
+                Expression: parser.Expression{
+                    Literal: parser.Literal{Value: 42},
+                },
+            }},
+        },
+    }
+    require.Equal(t, ast.Body, expectedAst)
 }
 
 func TestParser_Comment(t *testing.T) {
-    expected := 42
-    doc := fmt.Sprintf(
-`// this is a comment
+	expected := 42
+	doc := fmt.Sprintf(
+		`// this is a comment
        %d;`,
-       expected,
-    )
+		expected,
+	)
+	ast, err := parser.New().Parse(doc)
+	require.Nil(t, err)
+    expectedAst := parser.StatementList{
+        Statements: []parser.Statement{{
+            ExpressionStatement: &parser.ExpressionStatement{
+                Expression: parser.Expression{
+                    Literal: parser.Literal{Value: 42},
+                },
+            }},
+        },
+    }
+    require.Equal(t, ast.Body, expectedAst)
+}
+
+func TestParser_BlockStatement(t *testing.T) {
+    expected := 42
+    doc := fmt.Sprintf(`{
+  %d;
+}`, expected)
     ast, err := parser.New().Parse(doc)
     require.Nil(t, err)
-    lit := ast.Body.Statements[0].ExpressionStatement.Expression.Literal
-    require.Equal(t, lit.Type(), parser.IntLiteralType)
-    require.Equal(t, lit.Value(), expected)
+    expectedAst := parser.StatementList{
+        Statements: []parser.Statement{{
+            BlockStatement: &parser.BlockStatement{
+                StatementList: parser.StatementList{Statements: []parser.Statement{{
+                    ExpressionStatement: &parser.ExpressionStatement{
+                        Expression: parser.Expression{
+                            Literal: parser.Literal{Value: expected},
+                        },
+                    },
+                }}},
+            },
+        }},
+    }
+    require.Equal(t, ast.Body, expectedAst)
+}
+
+func TestParser_BlockStatementNested(t *testing.T) {
+    expected := 42
+    doc := fmt.Sprintf(`{
+  {42;}
+  {}
+}`)
+    ast, err := parser.New().Parse(doc)
+    require.Nil(t, err)
+    expectedAst := parser.StatementList{
+        Statements: []parser.Statement{{
+            BlockStatement: &parser.BlockStatement{
+                StatementList: parser.StatementList{Statements: []parser.Statement{{
+                    BlockStatement: &parser.BlockStatement{
+                        StatementList: parser.StatementList{Statements: []parser.Statement{{
+                            ExpressionStatement: &parser.ExpressionStatement{
+                                Expression: parser.Expression{
+                                    Literal: parser.Literal{Value: expected},
+                                },
+                            },
+                        }}},
+                    },
+                },{
+                    BlockStatement: &parser.BlockStatement{
+                        StatementList: parser.StatementList{},
+                    },
+                }}},
+            },
+        }},
+    }
+    require.Equal(t, ast.Body, expectedAst)
 }
