4de0bc2cdcd5e125e959fde009d46cfa4ca6dc84
Jack Homan <homan.jack@gmail.com>
2022-04-19 00:02:26 -0400

    support scripts

--- main.go
+++ main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"os"
 	"os/user"
 
+	"mitchlang/eval"
+	"mitchlang/lexer"
+	"mitchlang/object"
+	"mitchlang/parser"
 	"mitchlang/repl"
 )
 
 func main() {
+	flag.Parse()
+	if flag.NArg() > 0 {
+		raw, err := os.ReadFile(flag.Arg(0))
+		if err != nil {
+			panic(err)
+		}
+		script := string(raw)
+		l := lexer.New(script)
+		p := parser.New(l)
+		env := object.NewEnv()
+		obj := eval.Eval(p.ParseProgram(), env)
+		if obj.Type() == object.TypeError {
+			_, _ = io.WriteString(os.Stderr, obj.Inspect())
+			_, _ = io.WriteString(os.Stderr, "\n")
+			os.Exit(1)
+		}
+		if len(p.Errors()) > 0 {
+			for _, err := range p.Errors() {
+				_, _ = io.WriteString(os.Stderr, err)
+			}
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
 	u, err := user.Current()
 	if err != nil {
 		panic(err)
